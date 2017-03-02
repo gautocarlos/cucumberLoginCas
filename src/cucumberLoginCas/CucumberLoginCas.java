9319838d759d14122491c6c4a6dee80b2d8d2dc3
gautocarlos <cargauto@BUE-79QLQC2.usersad.everis.int>
2017-03-02 12:24:03 -0300

    Selenium - Wait
    Se agrega lógica de ejemplo para realizar espera de elementos en
    Selenium.

--- src/cucumberLoginCas/CucumberLoginCas.java
+++ src/cucumberLoginCas/CucumberLoginCas.java
@@ -2,12 +2,15 @@ package cucumberLoginCas;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Before;
+//import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -15,6 +18,7 @@ import cucumber.api.java.en.When;
 
 public class CucumberLoginCas {
 	WebDriver driver = null;
+	WebDriverWait wait;
 
 	@Given("^El usuario cuenta con credenciales existentes en GDE$")
 	public void openBrowser() {
@@ -33,6 +37,7 @@ public class CucumberLoginCas {
 	public void ingresoCAS(String urlCAS) {
 		System.out.println("URL: " + urlCAS);
 		driver.navigate().to(urlCAS);
+		wait = new WebDriverWait(driver, 10);
 	}
 
 	@When("^El usuario ingresa al sistema con sus credenciales: usuario \"(.*)\" y password \"(.*)\"$")
@@ -40,22 +45,29 @@ public class CucumberLoginCas {
 		System.out.println("Usuario: " + usuario);
 		System.out.println("Password: " + password);
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[2]/div/input")).clear();
-		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[2]/div/input"))
-				.sendKeys(usuario);
+		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[2]/div/input")).sendKeys(usuario);
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[3]/div/input")).clear();
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[3]/div/input"))
 				.sendKeys(password);
+//		WebElement element = wait.until(ExpectedConditions
+//				.elementToBeClickable(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[4]/button")));
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[4]/button")).click();
 	}
 
 	@Then("^El usuario ingresa a EU mediante la url \"(.*)\"$")
-	public void ingresoEU(String urlEU) {
+	public void ingresoEU(String urlEU) throws Exception {
 		System.out.println("urlEU: " + urlEU);
+		/** 
+		 * Aguarda que el logo de Gobierno esté cargado
+		 * */
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(
+				By.xpath("/html/body/div/div[1]/div/div/div[2]/table/tbody[2]/tr/td[1]/div/img")));
 		System.out.println("driver.getCurrentUrl(): " + driver.getCurrentUrl());
 		if (driver.getCurrentUrl().equalsIgnoreCase(urlEU)) {
 			System.out.println("TEST OK");
 		} else {
 			System.out.println("Test ERROR");
+			throw new Exception("ERROR - El usuario no llegó a la URL de EU");
 		}
 		driver.close();
 	}
